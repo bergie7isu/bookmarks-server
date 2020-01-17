6096bc685f5a18ee4e8b1479f0df06da26d4e28e
Mark Bergstrom <markbergstrom@gmail.com>
2020-01-17 00:23:14 -0600

    moved GET /bookmarks and /bookmarks/:id to database

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+insert into bookmarks (title, url, rating, description)
+values
+  ('Website 1', 'http://www.website1.com', '1', 'Cool website #1'),
+  ('Website 2', 'http://www.website2.com', '2', 'Cool website #2'),
+  ('Website 3', 'http://www.website3.com', '3', 'Cool website #3'),
+  ('Website 4', 'http://www.website4.com', '4', 'Cool website #4'),
+  ('Website 5', 'http://www.website5.com', '5', 'Cool website #5'),
+  ('Website 6', 'http://www.website6.com', '1', 'Cool website #6'),
+  ('Website 7', 'http://www.website7.com', '2', 'Cool website #7'),
+  ('Website 8', 'http://www.website8.com', '3', 'Cool website #8'),
+  ('Website 9', 'http://www.website9.com', '4', 'Cool website #9'),
+  ('Website 10', 'http://www.website10.com', '5', 'Cool website #10');
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,41 @@
+function makeBookmarksArray() {
+    return [
+        {
+            id: 1,
+            title: 'Website 1',
+            url: 'http://www.website1.com',
+            description: 'Cool website #1',
+            rating: 1
+        },
+        {
+            id: 2,
+            title: 'Website 2',
+            url: 'http://www.website2.com',
+            description: 'Cool website #2',
+            rating: 2
+        },
+        {
+            id: 3,
+            title: 'Website 3',
+            url: 'http://www.website3.com',
+            description: 'Cool website #3',
+            rating: 3
+        },
+        {
+            id: 4,
+            title: 'Website 4',
+            url: 'http://www.website4.com',
+            description: 'Cool website #4',
+            rating: 4
+        },
+        {
+            id: 5,
+            title: 'Website 5',
+            url: 'http://www.website5.com',
+            description: 'Cool website #5',
+            rating: 5
+        }
+    ];
+};
+
+module.exports = { makeBookmarksArray };
